253054934a20234d8949b9e6ba8656c4067a3d59
Donghyeon Lee <ehdgus001928@gmail.com>
2024-02-07 15:28:39 +0900

    feat: 페이지 제목 링크 추가
    - 로고 클릭 시 메인 페이지로 이동

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Title() {
-  return <h1 className="py-2 text-center text-4xl font-bold">HENOZIWD</h1>;
+  return (
+    <h1 className="py-2 text-center text-4xl font-bold">
+      <Link href="/">
+        HENOZIWD
+      </Link>
+    </h1>
+  );
 }
