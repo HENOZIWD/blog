79680c62baed2baf6b61ba596df305234f90ace2
Donghyeon Lee <ehdgus001928@gmail.com>
2024-03-25 01:08:27 +0900

    refactor: postList로 코드 통합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,10 @@
 import { getAllPostCard } from '@/lib/post';
 import PostList from '@/components/postList';
-import PostCard from '@/components/postCard';
 
 export default async function Home() {
   const posts = await getAllPostCard();
 
   return (
-    <PostList>
-      {posts.map(({
-        id, title, description, createdAt, thumbnail,
-      }, index) => (
-        <>
-          {index === 0
-            ? null
-            : (
-              <div
-                key={id}
-                className="h-0 w-full border-t border-t-newspaper-darkgray"
-              />
-            )}
-          <li key={id}>
-            <PostCard
-              id={id}
-              title={title}
-              description={description}
-              createdAt={createdAt}
-              thumbnail={thumbnail}
-            />
-          </li>
-        </>
-      ))}
-    </PostList>
+    <PostList posts={posts} />
   );
 }
--- src/components/postList.tsx
+++ src/components/postList.tsx
@@ -1,11 +1,37 @@
-export default function PostList({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import { Fragment } from 'react';
+import { PostCardProps } from '@/types/post';
+import PostCard from './postCard';
+
+interface PostListProps {
+  posts: PostCardProps[]
+}
+
+export default function PostList({ posts }: PostListProps) {
   return (
     <ul className="flex flex-col gap-8 max-lg:px-4">
-      {children}
+      {posts.map(({
+        id, title, description, createdAt, thumbnail,
+      }, index) => (
+        <Fragment key={id}>
+          {index === 0
+            ? null
+            : (
+              <div
+                key={`divline-${id}`}
+                className="h-0 w-full border-t border-t-newspaper-darkgray"
+              />
+            )}
+          <li key={id}>
+            <PostCard
+              id={id}
+              title={title}
+              description={description}
+              createdAt={createdAt}
+              thumbnail={thumbnail}
+            />
+          </li>
+        </Fragment>
+      ))}
     </ul>
   );
 }
